1d69e96313318e9ca10b67f70e82a049871f5e4d
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-18 09:20:54 -0300

    feat(utils): add currency formatting utility
    - Create `formatCurrency` function to format numbers to BRL currency
    
    This utility function will be used throughout the application to display
    monetary values consistently and in the correct format for Brazilian
    reals, improving user experience and data readability.

---
+++ src/utils/format.ts
@@ -0,0 +1,9 @@
+// função para formatar um número para moeda BRL
+const formatCurrency = (value: number) => {
+	return new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
+	}).format(value);
+};
+
+export { formatCurrency };
