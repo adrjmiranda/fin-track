f1e0576c51afc86507dba44a811efbebfb30ed43
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-20 12:37:24 -0300

    refactor: move useUserQueries hook to queries subdirectory

--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,7 +1,7 @@
 import type React from 'react';
 
 import { UserContext } from '@/contexts/UserContext';
-import { useUserQueries } from '@/hooks/useUserQueries';
+import { useUserQueries } from '@/hooks/queries/useUserQueries';
 
 const UserProvider = ({ children }: { children: React.ReactNode }) => {
 	const { user, isPending, balance, balanceIsPending } = useUserQueries();
